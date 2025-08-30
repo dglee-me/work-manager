4d6d9502fe981e15cac582f6d473b1c8b0199545
dglee-me <dglee.dev@gmail.com>
2025-08-30 15:52:35 +0900

    initialize

---
+++ src/components/landing/LandingPageHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar :elevation="0">
+    <v-app-bar-title class="font-weight-bold text-h5">Fide</v-app-bar-title>
+    <Divider />
+    <template #append>
+      <v-btn :ripple="false" slim="true" style="border-color: #C9CDCF;" variant="outlined">로그인 / 회원가입</v-btn>
+    </template>
+  </v-app-bar>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<route lang="yaml">
+meta:
+  layout: false
+</route>
+
+<template>
+  <LandingPageHeader />
+  <v-main>
+    <section class="abount-mentions d-flex justify-center text-h6">
+      <p>
+        "퇴사자에게 전화해야되나 걱정되시나요? 이젠
+        <span class="text-blue font-weight-bold">Fide</span>하세요."
+      </p>
+    </section>
+    <section class="image-section">
+      <v-img
+        alt="사람들이 책을 보는 이미지"
+        class="mx-auto"
+        src="@/assets/images/landing-img.webp"
+        width="80%"
+      />
+    </section>
+  </v-main>
+</template>
