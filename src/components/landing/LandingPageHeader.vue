d7c8cacab4824a82cf6aea6b250951c4783e14b0
dglee-me <dglee.dev@gmail.com>
2025-08-30 16:29:57 +0900

    feat: 로그인 1차 구현

--- src/components/landing/LandingPageHeader.vue
+++ src/components/landing/LandingPageHeader.vue
@@ -1,9 +1,18 @@
 <template>
   <v-app-bar :elevation="0">
-    <v-app-bar-title class="font-weight-bold text-h5">Fide</v-app-bar-title>
+    <v-app-bar-title class="font-weight-bold text-h5"
+      ><AppLogo
+    /></v-app-bar-title>
     <Divider />
     <template #append>
-      <v-btn :ripple="false" slim="true" style="border-color: #C9CDCF;" variant="outlined">로그인 / 회원가입</v-btn>
+      <v-btn
+        :ripple="false"
+        slim="true"
+        style="border-color: #c9cdcf"
+        to="/login"
+        variant="outlined"
+        >로그인 / 회원가입</v-btn
+      >
     </template>
   </v-app-bar>
 </template>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,14 @@
+<route lang="yaml">
+meta:
+  layout: false
+</route>
+
+<template>
+  <v-container>
+    <v-row class="logo-row">
+      <v-col class="d-flex justify-center" cols="12">
+        <AppLogo />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
